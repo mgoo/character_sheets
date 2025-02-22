b20dc40b719343364fed956183453a0ae0fa26b9
Abigail McGhie <andrewmcghienz@gmail.com>
2025-02-23 02:14:31 +1300

    added discovery mutation

--- js/constants.js
+++ js/constants.js
@@ -178,7 +178,7 @@ const void_stone_table = [
 const beneficial_mutations_co = [
     {
         name: 'Corebook',
-        desc: `Use the table in Numenera corebook.`,
+        desc: `Use the table in <b data-bs-toggle="offcanvas" href="#beneficialMutationPanel" role="button" aria-controls="beneficialMutationPanel">Numenera corebook</b>.`,
         weight: [1, 50]
     },
     {
@@ -255,8 +255,155 @@ const beneficial_mutations_co = [
 ]
 const beneficial_mutations = [
     {
-        name: '',
-        desc: ``,
+        name: 'Strengthened bones',
+        desc: `You gain +5 to your Might Pool.`,
+        weight: [1, 5]
+    },
+    {
+        name: 'Improved circulation',
+        desc: `You gain +5 to your Might Pool.`,
+        weight: [6, 10]
+    },
+    {
+        name: 'Improved musculature',
+        desc: `You gain +5 to your Might Pool.`,
+        weight: [11, 15]
+    },
+    {
+        name: 'Improved nervous system',
+        desc: `You gain +5 to your Speed Pool.`,
+        weight: [16, 20]
+    },
+    {
+        name: 'Improved neural processes',
+        desc: `You gain +5 to your Intellect Pool.`,
+        weight: [21, 25]
+    },
+    {
+        name: 'Thick hide',
+        desc: `You gain +1 to Armor.`,
+        weight: [26, 30]
+    },
+    {
+        name: 'Increased lung capacity:',
+        desc: `You can hold your breath for five minutes.`,
+        weight: [31, 33]
+    },
+    {
+        name: 'Adhesion pads',
+        desc: `Your hands and feet
+have naturally adhesive pads and thus are
+assets in tasks involving climbing, keeping
+your footing, or retaining your grip.`,
+        weight: [34, 36]
+    },
+    {
+        name: 'Slippery skin',
+        desc: `You secrete a slippery
+oil, giving you an asset in any task involving
+slipping from another’s grip, slipping from
+bonds, squeezing through a small opening,
+and so on.`,
+        weight: [37, 39]
+    },
+    {
+        name: 'Telekinetic shield',
+        desc: `You reflexively use
+telekinesis to ward away attacks, giving you an
+asset in Speed defense tasks.`,
+        weight: [40, 45]
+    },
+    {
+        name: 'Suggestive voice',
+        desc: `Your voice is so
+perfectly modulated that it is an asset in all
+interaction tasks.`,
+        weight: [46, 50]
+    },
+    {
+        name: 'Processor dreams',
+        desc: `When you sleep,
+you process information so that after you
+wake, you have an asset in any Intellect actions
+held over from the previous day. For example,
+if you have to determine whether an unknown
+plant is poisonous, you could “sleep on it” and
+make the determination the next day with an
+asset on the action.`,
+        weight: [51, 53]
+    },
+    {
+        name: 'Poison immunity',
+        desc: `You are immune to all poisons.`,
+        weight: [54, 60]
+    },
+    {
+        name: 'Disease immunity',
+        desc: `You are immune to all diseases.`,
+        weight: [61, 65]
+    },
+    {
+        name: 'Fire resistance',
+        desc: `You have +3 to Armor against damage from fire.`,
+        weight: [66, 70]
+    },
+    {
+        name: 'Cold resistance',
+        desc: `You have +4 to Armor against damage from cold.`,
+        weight: [71, 75]
+    },
+    {
+        name: 'Psychic resistance',
+        desc: `You have +3 to Armor against Intellect damage.`,
+        weight: [76, 80]
+    },
+    {
+        name: 'Acid resistance',
+        desc: `You have +5 to Armor against damage from acid.`,
+        weight: [81, 85]
+    },
+    {
+        name: 'Puncture resistance',
+        desc: `You have +2 to Armor against damage from puncturing attacks.`,
+        weight: [86, 88]
+    },
+    {
+        name: 'Slicing resistance',
+        desc: `You have +2 to Armor against damage from slicing attacks.`,
+        weight: [89, 91]
+    },
+    {
+        name: 'Bludgeoning resistance',
+        desc: `You have +2 to Armor against damage from crushing attacks.`,
+        weight: [92, 94]
+    },
+    {
+        name: 'No scent',
+        desc: `You cannot be tracked or located by scent, and you never have offensive odors.`,
+        weight: [95, 96]
+    },
+    {
+        name: 'Scent',
+        desc: `You can sense creatures,
+objects, and terrain by scent as well as a
+normal human can by sight. You can detect
+scents with that degree of accuracy only in
+short range, but you can sense strong odors
+from much farther away (far better than a
+normal human can). Like a hound, you can
+track creatures by their scent.`,
+        weight: [97, 99]
+    },
+    {
+        name: 'Sense Material',
+        desc: `You can sense the
+presence of any single substance within short
+range, although you don’t learn details or the
+precise location. You and the GM should work
+together to determine the substance: water,
+iron, synth, granite, wood, flesh, salt, and so
+on. You do not need to concentrate to sense
+the material.`,
         weight: [0, 0]
     }
 ]
@@ -264,7 +411,7 @@ const beneficial_mutations = [
 const harmful_mutations_co = [
     {
         name: 'Corebook',
-        desc: `Use table in Numenera corebook`,
+        desc: `Use table in <b data-bs-toggle="offcanvas" href="#harmfulMutationPanel" role="button" aria-controls="harmfulMutationPanel">Numenera corebook</b>.`,
         weight: [1, 70]
     },
     {
@@ -333,13 +480,96 @@ const harmful_mutations_co = [
         weight: [0, 0]
     }
 ]
+const harmful_mutations = [
+    {
+        name: 'Deformed leg',
+        desc: `All movement tasks are hindered.`,
+        weight: [1, 10]
+    },
+    {
+        name: 'Deformed face/appearance',
+        desc: `All pleasant interaction tasks are hindered.`,
+        weight: [11, 20]
+    },
+    {
+        name: 'Deformed arm/hand',
+        desc: `All tasks involving the arm or hand are hindered.`,
+        weight: [21, 30]
+    },
+    {
+        name: 'Malformed brain',
+        desc: `All memory or cognitive-related tasks are hindered.`,
+        weight: [31, 40]
+    },
+    {
+        name: 'Mentally vulnerable',
+        desc: `All Intellect defense tasks are hindered.`,
+        weight: [41, 45]
+    },
+    {
+        name: 'Slow and lumbering:',
+        desc: `All Speed defense tasks are hindered.`,
+        weight: [46, 50]
+    },
+    {
+        name: 'Sickly',
+        desc: `All Might defense tasks are hindered.`,
+        weight: [51, 60]
+    },
+    {
+        name: 'Horrible growth',
+        desc: `A large goiter,
+immobile tendril, or useless extra eye hangs from
+your face, hindering all pleasant interactions
+(with most creatures, particularly humans).`,
+        weight: [61, 63]
+    },
+    {
+        name: 'Useless limb',
+        desc: `One of your limbs is unusable or missing.`,
+        weight: [64, 66]
+    },
+    {
+        name: 'Useless eye',
+        desc: `One of your eyes is unusable or missing. Tasks specifically involving eyesight (spotting, searching, and so on) are hindered.`,
+        weight: [67, 71]
+    },
+    {
+        name: 'Useless ear',
+        desc: `One of your ears is
+unusable or missing. Tasks specifically
+involving hearing are hindered.`,
+        weight: [72, 76]
+    },
+    {
+        name: 'Weakness in Might',
+        desc: `Any time you
+spend points from your Might Pool, the cost is
+increased by 1 point.`,
+        weight: [77, 84]
+    },
+    {
+        name: 'Weakness in Speed',
+        desc: `Any time you
+spend points from your Speed Pool, the cost is
+increased by 1 point.`,
+        weight: [85, 92]
+    },
+    {
+        name: 'Weakness in Intellect',
+        desc: `Any time you
+spend points from your Intellect Pool, the cost
+is increased by 1 point.`,
+        weight: [93, 0]
+    },
 
+]
 
 
 const powerful_mutations_co = [
     {
         name: 'Corebook',
-        desc: `Use the table in the Numenera corebook`,
+        desc: `Use the table in the <b data-bs-toggle="offcanvas" href="#powerfulMutationPanel" role="button" aria-controls="powerfulMutationPanel">Numenera corebook</b>.`,
         weight: [1, 50]
     },
     {
@@ -694,10 +924,271 @@ carry you aloft like a bird’s wings. Enabler.`,
 ]
 const powerful_mutations = [
     {
-        name: '',
-        desc: ``,
+        name: 'Darksight',
+        desc: `You can see in complete darkness as if it were light. Enabler.`,
+        weight: [1, 5]
+    },
+    {
+        name: 'No breath',
+        desc: `You do not need to breathe. Enabler.`,
+        weight: [6, 10]
+    },
+    {
+        name: 'No water',
+        desc: `You do not need to drink water to survive. Enabler.`,
+        weight: [11, 15]
+    },
+    {
+        name: 'Chameleon skin',
+        desc: `Your skin changes
+colors as you wish. This is an asset in tasks
+involving hiding. Enabler.`,
+        weight: [16, 20]
+    },
+    {
+        name: 'Savage bite',
+        desc: `Your mouth widens
+surprisingly, and hidden, pointed teeth emerge
+when you wish it. You can make a bite attack
+that inflicts 3 points of damage. Enabler.`,
+        weight: [21, 24]
+    },
+    {
+        name: 'Gluey globs',
+        desc: `You can produce gluey
+globs at your fingertips. This is an asset in
+tasks involving climbing or keeping your grip.
+You can also fling these globs in immediate
+range, and if they hit, they hinder the target’s
+physical tasks for one round. Enabler to use in
+a task; action to use as an attack.`,
+        weight: [25, 26]
+    },
+    {
+        name: 'Face dancing',
+        desc: `You can alter your
+features enough to give you an asset in all
+tasks involving disguise. Enabler.`,
+        weight: [27, 30]
+    },
+    {
+        name: 'Sense numenera',
+        desc: `You can sense the
+presence of a functioning numenera device or
+special ability within short range. You do not
+learn details or the precise location. Action.`,
+        weight: [31, 35]
+    },
+    {
+        name: 'Stinger in finger',
+        desc: `You can make an
+attack with your hand that inflicts 1 point of
+damage. If you make a second successful
+attack roll, your stinger also injects a poison
+that inflicts 4 points of Speed damage. Action.`,
+        weight: [36, 40]
+    },
+    {
+        name: 'Stinger in elbow:',
+        desc: `You can make an
+attack with your elbow that inflicts 2 points
+of damage. If you make a second successful
+attack roll, your stinger also injects a poison
+that inflicts 4 points of Speed damage. Action.`,
+        weight: [41, 44]
+    },
+    {
+        name: 'Spit needles',
+        desc: `You can make an attack
+with immediate range. You spit a needle
+that inflicts 1 point of damage. If you make a
+second successful attack roll, the needle also
+injects a poison that inflicts 4 points of Speed
+damage. Action.`,
+        weight: [45, 47]
+    },
+    {
+        name: 'Spit acid:',
+        desc: `You can make an attack with
+immediate range. You spit a glob of acid that
+inflicts 2 points of damage. Action.`,
+        weight: [48, 50]
+    },
+    {
+        name: 'Spit webs',
+        desc: `You can make up to 10 feet
+(3 m) of a strong, ropelike material each day
+at the rate of about 1 foot (30 cm) per minute.
+The webbing is level 3. You can also spit globs
+of webbing in immediate range, and if they hit,
+the target’s physical tasks are hindered for one
+round. Action.`,
+        weight: [51, 53]
+    },
+    {
+        name: 'Filtered lungs',
+        desc: `You have an asset
+to Might defense actions against vapors or
+noxious gases. You can survive in a hostile
+breathing environment (such as underwater or
+in a vacuum) for up to ten minutes. Enabler.`,
+        weight: [54, 59]
+    },
+    {
+        name: 'Disruptive field (electronics) (2 Int)',
+        desc: `When you wish it, you disrupt
+devices within immediate range (no roll
+needed). All devices operate as if they were
+3 levels lower while in range of your field.
+Devices reduced to level 0 or below do not
+function. Action.`,
+        weight: [60, 62]
+    },
+    {
+        name: 'Disruptive field (flesh) (2 Int):',
+        desc: `When you wish it, you disrupt flesh
+within immediate range. All creatures within
+range of your field take 1 point of damage.
+If you apply a level of Effort to increase the
+damage rather than affect the difficulty, each
+target takes 2 additional points of damage. If
+your attack fails, targets in the area still take 1
+point of damage. Action.`,
+        weight: [63, 65]
+    },
+    {
+        name: 'Disruptive field (thoughts) (1 Int)',
+        desc: `When you wish it, you disrupt thoughts
+within immediate range. Intellect actions for
+all creatures within range are hindered. Action.`,
+        weight: [66, 68]
+    },
+    {
+        name: 'Magnetic flesh',
+        desc: `You attract or repel
+metal when you desire. Not only do small
+metal objects cling to you, but this mutation is
+an asset in tasks involving climbing on metal
+or keeping your grip on a metal item. This
+mutation is an asset to Speed defense tasks
+when being attacked by a metal foe or a foe
+with a metal weapon. Enabler.`,
+        weight: [69, 70]
+    },
+    {
+        name: 'Gravity negation (2 Int)',
+        desc: `You float slowly into the air. If you concentrate,
+you can control your movement at half your
+normal speed; otherwise, you drift with the
+wind or with any momentum you have gained.
+This effect lasts for up to ten minutes. Action
+to initiate.`,
+        weight: [71, 73]
+    },
+    {
+        name: 'Telepathy (2 Int):',
+        desc: `You can
+speak telepathically with others who are within
+short range. Communication is two-way, but
+the other party must be willing and able to
+communicate. You don’t have to see the target,
+but you must know that it’s within range. You
+can have more than one active contact at
+once, but you must establish contact with each
+target individually. Each contact lasts up to ten
+minutes. In addition to the normal options for
+using Effort, you can choose to use a level of
+Effort to increase the duration of contact to 28
+hours. Action to establish contact.`,
+        weight: [74, 80]
+    },
+    {
+        name: 'Pyrokinesis (1 Int)',
+        desc: `You can
+cause a flammable object you can see within
+immediate range to spontaneously catch fire.
+If used as an attack, this power inflicts 2 points
+of damage. Action.`,
+        weight: [81, 85]
+    },
+    {
+        name: 'Telekinesis (2 Int):',
+        desc: `You
+can exert force on objects within short range.
+Once activated, your power has an effective
+Might Pool of 10, a Might Edge of 1, and an
+Effort of 2 (approximately equal to the strength
+of a fit, capable, adult human), and you can
+use it to move objects, push against objects,
+and so on. For example, you could lift and
+pull a light object anywhere within range to
+yourself or move a heavy object (like a piece
+of furniture) about 10 feet (3 m). This power
+lacks the fine control to wield a weapon or
+move objects with much speed, so in most
+situations, it’s not a means of attack. You can’t
+use this ability on your own body. The power
+lasts for one hour or until its Might Pool is
+depleted, whichever comes first. Action.`,
+        weight: [86, 90]
+    },
+    {
+        name: 'Phaseshifting (2 Int)',
+        desc: `You
+can pass slowly through solid barriers at a rate
+of 1 inch (2.5 cm) per round (minimum of one
+round to pass through the barrier). You can’t
+act (other than moving) or perceive anything
+until you pass entirely through the barrier. You
+can’t pass through energy barriers. Action.`,
+        weight: [91, 92]
+    },
+    {
+        name: 'Power device (1+ Int)',
+        desc: `You
+can charge an artifact or other device (except
+a cypher) so that it can be used once. The cost
+is 1 Intellect point plus 1 point per level of the
+device. Action.`,
+        weight: [93, 94]
+    },
+    {
+        name: 'Drain power',
+        desc: `You can drain the power
+from an artifact or device, allowing you to
+regain 1 Intellect point per level of the device.
+You regain points at the rate of 1 point per
+round and must give your full concentration to
+the process each round. The GM determines
+whether the device is fully drained (likely
+true of most handheld or smaller devices)
+or retains some power (likely true of large
+machines). Action to initiate; action each
+round to drain.`,
+        weight: [95, 96]
+    },
+    {
+        name: 'Regeneration',
+        desc: `In addition to regaining
+points through normal recovery rolls, you
+regain 1 point of your Might Pool or Speed
+Pool per hour, regardless of whether you
+rest, until both Pools are at their maximum.
+Enabler.`,
+        weight: [97, 99]
+    },
+    {
+        name: 'Feed off pain',
+        desc: `Any time a creature within
+immediate range suffers at least 3 points of
+damage (after Armor subtraction) in one attack,
+you can restore 1 point to one of your Pools, up
+to its maximum. You can feed off any creature in
+this way, whether friend or foe. You never regain
+more than 1 point per round. Enabler.`,
         weight: [0, 0]
-    }
+    },
+
 ]
 
 
